f22d1b31d415816371021e11f3459add578aac72
Vincent A <71930091+VincentA55@users.noreply.github.com>
2021-02-15 14:36:43 +0000

    trying to add test

---
+++ ConsoleApp.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApp.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
