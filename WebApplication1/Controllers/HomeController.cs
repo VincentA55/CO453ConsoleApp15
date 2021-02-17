68e2adcaa6900fa0ccbcec1f886a7dbd640c2ab6
Vincent A <71930091+VincentA55@users.noreply.github.com>
2021-02-17 17:19:45 +0000

    adding distanceconverter view

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -11,14 +11,12 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DistanceConverter()
         {
             return View();
         }
