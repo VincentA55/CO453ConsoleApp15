d4d031202bf3d48a24e817a55cd685397c27e41e
Vincent A <71930091+VincentA55@users.noreply.github.com>
2021-03-05 16:19:29 +0000

    test driven development

---
+++ ConsoleApp.UnitTest/TestStudentGrades.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppProject.App03;
+
+namespace ConsoleApp.UnitTest
+{
+    [TestClass]
+    public class TestStudentGrades
+    {
+       private readonly StudentGrades studentGrades = new StudentGrades();
+
+        [TestMethod]
+        public void Convert0ToGradeF()
+        {
+            // Arrange
+            Grades expectedGrade = Grades.F;
+
+            // Act
+            Grades actualGrade = studentGrades.ConvertToGrade(0);
+
+            // Assert
+            Assert.AreEqual(expectedGrade, actualGrade);
+        }
+    }
+}
--- ConsoleAppProject/App03/StudentGrades.cs
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -1,10 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace ConsoleAppProject.App03
 {
-    class StudentGrades
+    public class StudentGrades
     {
+        public string[] Students { get; set; }
+
+        public int[] Marks { get; set; }
+
+        public int[] GradeProfile { get; set; }
+
+        public double Mean { get; set; }
+
+        public int Minimum { get; set; }
+
+        public int Maximum { get; set; }
+
+        /// <summary>
+        /// Class constructor sets up an array of students
+        /// </summary>
+        public StudentGrades()
+        {
+            Students = new string[]
+            {
+                "Daniel","Janiel", "Shmaniel",
+                "Billy", "Georgia", "Roman",
+                "Wills", "Fabio", "Oliver",
+                "LisaLisa"
+            };
+
+            GradeProfile = new int[(int)Grades.A + 1];
+            Marks = new int[Students.Length];
+        }
+
+        /// <summary>
+        /// Input a mark between 0-100 for each
+        /// student and stores it in the Marks array
+        /// </summary>
+        public void InputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Lists all the students names and current mark
+        /// </summary>
+        public void OutputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Convert a students marks into a Grade(A-F)
+        /// </summary>
+        /// <param name="mark"></param>
+        /// <returns>Grades</returns>
+        public Grades ConvertToGrade(int mark)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Calculate and displays 
+        /// min, max, and mean mark for all students
+        /// </summary>
+        public void CalculateStats()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
