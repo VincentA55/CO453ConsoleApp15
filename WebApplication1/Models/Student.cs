a54bb52880ad837c7ef92b52353312f502d80ce4
Vincent A <71930091+VincentA55@users.noreply.github.com>
2021-03-12 16:22:55 +0000

    adding web end for app03

---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WebApplication1.Models
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+
+        [StringLength(20), Required]
+        public string Name { get; set; }
+
+        [Range(0,100)]
+        public int Mark { get; set; }
+
+    }
+}
