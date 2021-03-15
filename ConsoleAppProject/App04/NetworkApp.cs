10bf7c7ebd35865d4332b3d968da5ff50f026969
Vincent A <71930091+VincentA55@users.noreply.github.com>
2021-03-15 17:36:03 +0000

    Create NetworkApp.cs

---
+++ ConsoleAppProject/App04/NetworkApp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppProject.App04
+{
+   public class NetworkApp
+    {
+        public void DisplayMenu()
+        {
+            ConsoleHelper.OutputHeading("   Vinnys News Feed  ");
+
+            string[] choices = new string[]
+            {
+                "Post Message", "Post Image", "DisplayMenu All Posts"
+                ,"Quit"
+            };
+
+            bool finished = false;
+
+            do
+            {
+                switch (ConsoleHelper.SelectChoice(choices))
+                {
+                    case 1:
+                        PostMessage();
+                        break;
+
+                    case 2:
+                        PostImage();
+                        break;
+
+                    case 3:
+                        DisplayAll();
+                        break;
+
+                    case 4:
+                        finished = true;
+                        break;
+                }
+            }
+            while (!finished);
+        }
+    }
+}
