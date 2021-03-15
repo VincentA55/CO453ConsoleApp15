810a846929ef68fa422a39f13d9d344cdedfbb7c
Vincent A <71930091+VincentA55@users.noreply.github.com>
2021-03-15 16:42:22 +0000

    refactoing posts app04

--- ConsoleAppProject/App04/Post.cs
+++ ConsoleAppProject/App04/Post.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace ConsoleAppProject.App04
 {
@@ -9,7 +8,6 @@ namespace ConsoleAppProject.App04
         private int likes;
 
         private readonly List<String> comments;
-
 
         // username of the post's author
         public String Username { get; }
@@ -24,7 +22,6 @@ namespace ConsoleAppProject.App04
         {
             this.Username = author;
             Timestamp = DateTime.Now;
-
 
             likes = 0;
             comments = new List<String>();
@@ -54,10 +51,71 @@ namespace ConsoleAppProject.App04
         /// </summary>
         /// <param name="text">
         /// The new comment to add.
-        /// </param>        
+        /// </param>
         public void AddComment(String text)
         {
             comments.Add(text);
+        }
+
+        ///<summary>
+        /// Display the details of this post.
+        ///
+        /// (Currently: Print to the text terminal. This is simulating display
+        /// in a web browser for now.)
+        ///</summary>
+        public void Display()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"    Author: {Username}");
+            Console.WriteLine($"    Time Elpased: {FormatElapsedTime(Timestamp)}");
+            Console.WriteLine();
+
+            if (likes > 0)
+            {
+                Console.WriteLine($"    Likes:  {likes}  people like this.");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            if (comments.Count == 0)
+            {
+                Console.WriteLine("    No comments.");
+            }
+            else
+            {
+                Console.WriteLine($"    {comments.Count}  comment(s). Click here to view.");
+            }
+        }
+
+        ///<summary>
+        /// Create a string describing a time point in the past in terms
+        /// relative to current time, such as "30 seconds ago" or "7 minutes ago".
+        /// Currently, only seconds and minutes are used for the string.
+        /// </summary>
+        /// <param name="time">
+        ///  The time value to convert (in system milliseconds)
+        /// </param>
+        /// <returns>
+        /// A relative time string for the given time
+        /// </returns>
+        private String FormatElapsedTime(DateTime time)
+        {
+            DateTime current = DateTime.Now;
+            TimeSpan timePast = current - time;
+
+            long seconds = (long)timePast.TotalSeconds;
+            long minutes = seconds / 60;
+
+            if (minutes > 0)
+            {
+                return minutes + " minutes ago";
+            }
+            else
+            {
+                return seconds + " seconds ago";
+            }
         }
     }
 }
