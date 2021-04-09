c0b1ce42ad90403f5cee6832a938cc10d98995a3
Vincent A <71930091+VincentA55@users.noreply.github.com>
2021-04-09 16:27:34 +0100

    Create Animation.cs

---
+++ App05/Models/Animation.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App05.Models
+{
+   public class Animation
+    {
+        public int CurrentFrame { get; set; }
+
+        public int FrameCount { get; private set; }
+
+        public int FrameHeight { get { return Texture.Height; } }
+
+        public int FrameWidth { get { return Texture.Width; } }
+        
+        public float FrameSpeed { get; set; }
+
+        public bool IsLooping { get; set; }
+
+        public Texture2D Texture { get; private set; }
+
+        public Animation (Texture2D texture, int frameCount)
+        {
+
+        }
+
+
+    }
+}
