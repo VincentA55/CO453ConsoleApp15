1c1563174ac5d33555d9e430c9d3b7feb3993ff7
Vincent A <71930091+VincentA55@users.noreply.github.com>
2021-05-19 15:19:57 +0100

    working on game states

---
+++ App05/States/GameState.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App05.States
+{
+    class GameState
+    {
+    }
+}
---
+++ App05/States/MenuState.cs
@@ -0,0 +1,45 @@
+﻿using App05.Menus;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App05.States
+{
+    public class MenuState : State
+    {
+        private List<Component> _components;
+
+        public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content)
+            :base(game, graphicsDevice, content)
+        {
+            var buttonTexture = _content.Load<Texture2D>("BasicButton");
+            var buttonFont = _content.Load<SpriteFont>("Font");
+
+            var newGameButton = new Button(buttonTexture, buttonFont);
+
+            _components = new List<Component>()
+            {
+
+
+            };
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
